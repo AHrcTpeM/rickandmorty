cbbf0fccd8fff194804ac29a0dd5b06b92b21832
ipopkov <igorpopkov86@gmail.com>
2023-03-15 10:46:19 +0200

    add google session

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -4,37 +4,48 @@ import './auth.scss';
 import SignOut from './signout'
 
 function Auth() {
+    const [token] = useState(sessionStorage.getItem("token"));
     const [user, setUser] = useState({});
 
     function handleCredentialResponse(response) {
+        sessionStorage.setItem("token", response.credential);
         let userObj = jwt_decode(response.credential);
         setUser(userObj);
         document.getElementById('google_btn').hidden = true;
     }
 
     function handleSignOut(event) {
+        sessionStorage.setItem("token", "");
         window.google.accounts.id.disableAutoSelect();
         setUser({});
         document.getElementById('google_btn').hidden = false;
     }
 
     useEffect(() => {
-    window.google.accounts.id.initialize({
-        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        callback: handleCredentialResponse
-    });
-    const parent = document.getElementById('google_btn');
-    window.google.accounts.id.renderButton(parent, {theme: "outline"});
-    window.google.accounts.id.prompt();
-    }, []);    
+        window.google.accounts.id.initialize({
+            client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+            callback: handleCredentialResponse
+        });
+        const parent = document.getElementById('google_btn');
+        window.google.accounts.id.renderButton(parent, {theme: "outline"});
+        
+        try {
+            const data = jwt_decode(token);
+            setUser(data);            
+            document.getElementById('google_btn').hidden = true;
+        } catch(error) {
+            // console.log('Invalid token specified');
+            window.google.accounts.id.prompt();
+        }
+    }, [token]);    
 
     return (
         <>
-            <div id='google_btn'></div>            
+            <div id='google_btn'></div>     
             { user.name && 
                 <>
                     <div id='googleFrame' className='gFrame' onClick={(e) => handleSignOut(e)}>
-                        <img className='gImage' src={user.picture ?? 'images/google_user.jpg'} alt="alt"></img>
+                        <img className='gImage' src={user.picture ?? 'images/google_user.jpg'} alt="avatar"></img>
                         <div className='flexCenter'>
                             <div id='gName'>{user.name}</div>
                         </div>
